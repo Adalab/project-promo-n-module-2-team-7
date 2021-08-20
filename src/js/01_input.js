cda11e314fa959d6cae8bee5913ffef0ab09d853
patricuismart <patricuismart@gmail.com>
2021-08-20 13:54:37 +0200

    Include conditional if input.value empty

--- src/js/01_input.js
+++ src/js/01_input.js
@@ -1,12 +1,17 @@
 'use strict';
 
 //elementos HTML
-
 const nameElement = document.querySelector('.js_name'); //clase en fill.html en input
 const namePreviewElement = document.querySelector('.js_preview-name'); //clase en hero.html en h2
 console.log(nameElement.value);
+
 function handleName() {
-  namePreviewElement.innerHTML = nameElement.value;
+  if (!nameElement.value) {
+    namePreviewElement.innerHTML = 'Nombre Apellido';
+  } else {
+    console.log(nameElement.value);
+    namePreviewElement.innerHTML = nameElement.value;
+  }
 }
 
 nameElement.addEventListener('keyup', handleName);
