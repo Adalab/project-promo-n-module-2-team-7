3285fc7d4cfd4a0089c539b7a3876cd9b408d7de
AndreaAguado <andrea95am@gmail.com>
2021-09-03 14:06:35 +0200

    add form in js

--- src/js/01_input.js
+++ src/js/01_input.js
@@ -5,13 +5,34 @@ const nameElement = document.querySelector('.js_name'); //clase en fill.html en 
 const namePreviewElement = document.querySelector('.js_preview-name'); //clase en hero.html en h2
 console.log(nameElement.value);
 
-function handleName() {
+let data = {
+  palette: '',
+  fullName: '',
+  jobPosition: '',
+  email: '',
+  phone: '',
+  linkedin: '',
+  github: ''
+};
+const form = document.querySelector('.profilecards__app');
+
+function handleName(ev) {
+ 
+  const inputId = ev.target.id;
+  const inputValue = ev.target.value;
+  console.log(inputId, inputValue);
+
+  data[inputId] = inputValue;
+  console.log(data);
+
+
   if (!nameElement.value) {
-    namePreviewElement.innerHTML = 'Nombre Apellido';
+    inputId.innerHTML = inputValue;
+
   } else {
     console.log(nameElement.value);
     namePreviewElement.innerHTML = nameElement.value;
   }
 }
 
-nameElement.addEventListener('keyup', handleName);
+form.addEventListener('change', handleName);
