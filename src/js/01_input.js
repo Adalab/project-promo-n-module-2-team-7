7ca1b2e917ddd1fa76907b84175a35ea126a86cb
AndreaAguado <andrea95am@gmail.com>
2021-09-05 18:11:45 +0200

    fill card

--- src/js/01_input.js
+++ src/js/01_input.js
@@ -3,6 +3,12 @@
 //elementos HTML
 const nameElement = document.querySelector('.js_name'); //clase en fill.html en input
 const namePreviewElement = document.querySelector('.js_preview-name'); //clase en hero.html en h2
+
+// const jobPreview = document.querySelector('.js_preview-job');
+
+const cardElements = document.querySelectorAll('.js_preview');
+
+console.log(cardElements);
 console.log(nameElement.value);
 
 let data = {
@@ -16,7 +22,7 @@ let data = {
 };
 const form = document.querySelector('.profilecards__app');
 
-function handleName(ev) {
+function handleFillData(ev) {
  
   const inputId = ev.target.id;
   const inputValue = ev.target.value;
@@ -25,14 +31,33 @@ function handleName(ev) {
   data[inputId] = inputValue;
   console.log(data);
 
+  fillCard(data);
 
-  if (!nameElement.value) {
-    inputId.innerHTML = inputValue;
-
-  } else {
-    console.log(nameElement.value);
-    namePreviewElement.innerHTML = nameElement.value;
-  }
 }
 
-form.addEventListener('change', handleName);
+function fillCard(data){
+  console.log('estoy aqui');
+  if(data.fullName !== ''){
+    cardElements[0].innerHTML = data.fullName;
+  }
+  else if (data.jobPosition !== ''){
+    cardElements[1].innerHTML = data.jobPosition;
+  }
+  else if(data.phone !== ''){
+    cardElements[2].innerHTML = data.phone;
+  }
+  else if(data.email !== ''){
+    cardElements[3].innerHTML = data.email;
+  }
+  else if(data.linkedin !== ''){
+    cardElements[4].innerHTML = data.linkedin;
+  }
+  else if(data.github !== ''){
+    let href= 'https://github.com/' + data.github ;
+    cardElements[5].setAttribute('.href', href );
+  }
+
+}
+
+
+form.addEventListener('change', handleFillData);
