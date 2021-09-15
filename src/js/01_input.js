fa18c0b876c1f5aac95e414cc5c743ccda1f4557
Claudia Stein <claudiasantanastein@hotmail.com>
2021-09-15 17:29:24 +0200

    personalized css added

--- src/js/01_input.js
+++ src/js/01_input.js
@@ -1,29 +1,29 @@
-'use strict';
+"use strict";
 
 //elementos HTML
 // const nameElement = document.querySelector('.js_name'); //clase en fill.html en input
 // const namePreviewElement = document.querySelector('.js_preview-name'); //clase en hero.html en h2
 
-const jobPreview = document.querySelector('.js_preview-job');
+// const jobPreview = document.querySelector('.js_preview-job');
 
-const cardElements = document.querySelectorAll('.js_preview');
+const cardElements = document.querySelectorAll(".js_preview");
 
-const cardName = document.querySelector('.js-name');
-const cardRectangle = document.querySelector('.js-rectangle');
+const cardName = document.querySelector(".js-name");
+const cardRectangle = document.querySelector(".js-rectangle");
 
-const cardIcons = document.querySelectorAll('.js-icon');
+const cardIcons = document.querySelectorAll(".js-icon");
 
 console.log(cardElements);
 // console.log(nameElement.value);
 
 let data = {
-  palette: '',
-  name: '',
-  job: '',
-  email: '',
-  phone: '',
-  linkedin: '',
-  github: '',
+  palette: "",
+  name: "",
+  job: "",
+  email: "",
+  phone: "",
+  linkedin: "",
+  github: "",
 };
 
 // let paletteColors = [
@@ -47,10 +47,10 @@ let data = {
 // for (const item of paletteColors) {
 
 // }
-const form = document.querySelector('.profilecards__app');
+const form = document.querySelector(".profilecards__app");
 
 function handleFillData(ev) {
-  if (ev.target.name === 'palette') {
+  if (ev.target.name === "palette") {
     const inputName = ev.target.name;
     const inputValue = ev.target.value;
     console.log(inputName, inputValue);
@@ -69,102 +69,102 @@ function handleFillData(ev) {
 }
 
 function fillCard(data) {
-  console.log('estoy aqui');
-  if (data.job !== '') {
-    cardElements[1].innerHTML = data.job;
-  } else if (data.name !== '') {
+  console.log("estoy aqui");
+  if (data.name !== "") {
     cardElements[0].innerHTML = data.name;
-  } else if (data.phone !== '') {
+  } else if (data.job !== "") {
+    cardElements[1].innerHTML = data.job;
+  } else if (data.phone !== "") {
     cardElements[2].innerHTML = data.phone;
-  } else if (data.email !== '') {
+  } else if (data.email !== "") {
     cardElements[3].innerHTML = data.email;
-  } else if (data.linkedin !== '') {
+  } else if (data.linkedin !== "") {
     cardElements[4].innerHTML = data.linkedin;
-  } else if (data.github !== '') {
-    let href = 'https://github.com/' + data.github;
-    cardElements[5].setAttribute('href', href);
+  } else if (data.github !== "") {
+    let href = "https://github.com/" + data.github;
+    cardElements[5].setAttribute("href", href);
   }
 }
 
 function applyColor(data) {
-  console.log('estoy en los colores');
+  console.log("estoy en los colores");
 
-  if (data.palette === '1') {
+  if (data.palette === "1") {
     //cardName es el nombre
-    cardName.classList.remove('color1');
-    cardName.classList.remove('color4');
-    cardName.classList.remove('color7');
-    cardName.classList.add('color1');
+    cardName.classList.remove("color1");
+    cardName.classList.remove("color4");
+    cardName.classList.remove("color7");
+    cardName.classList.add("color1");
     //cardRectangle es el palito
-    cardRectangle.classList.remove('color2');
-    cardRectangle.classList.remove('color5');
-    cardRectangle.classList.remove('color8');
-    cardRectangle.classList.add('color2');
+    cardRectangle.classList.remove("color2");
+    cardRectangle.classList.remove("color5");
+    cardRectangle.classList.remove("color8");
+    cardRectangle.classList.add("color2");
     //icons son los iconos
     for (const icon of cardIcons) {
-      console.log('entra al for');
+      console.log("entra al for");
       //color del icono
-      icon.classList.remove('color1');
-      icon.classList.remove('color4');
-      icon.classList.remove('color7');
-      icon.classList.add('color1');
+      icon.classList.remove("color1");
+      icon.classList.remove("color4");
+      icon.classList.remove("color7");
+      icon.classList.add("color1");
       //color del borde del icono
-      icon.classList.remove('color3');
-      icon.classList.remove('color6');
-      icon.classList.remove('color9');
-      icon.classList.add('color3');
+      icon.classList.remove("color3");
+      icon.classList.remove("color6");
+      icon.classList.remove("color9");
+      icon.classList.add("color3");
     }
-  } else if (data.palette === '2') {
-    cardName.classList.remove('color1');
-    cardName.classList.remove('color4');
-    cardName.classList.remove('color7');
-    cardName.classList.add('color4');
+  } else if (data.palette === "2") {
+    cardName.classList.remove("color1");
+    cardName.classList.remove("color4");
+    cardName.classList.remove("color7");
+    cardName.classList.add("color4");
     //cardRectangle es el palito
-    cardRectangle.classList.remove('color2');
-    cardRectangle.classList.remove('color5');
-    cardRectangle.classList.remove('color8');
-    cardRectangle.classList.add('color5');
+    cardRectangle.classList.remove("color2");
+    cardRectangle.classList.remove("color5");
+    cardRectangle.classList.remove("color8");
+    cardRectangle.classList.add("color5");
     //icons son los iconos
     for (const icon of cardIcons) {
-      console.log('entra al for');
+      console.log("entra al for");
       //color del icono
-      icon.classList.remove('color1');
-      icon.classList.remove('color4');
-      icon.classList.remove('color7');
-      icon.classList.add('color4');
+      icon.classList.remove("color1");
+      icon.classList.remove("color4");
+      icon.classList.remove("color7");
+      icon.classList.add("color4");
       //color del borde del icono
-      icon.classList.remove('color3');
-      icon.classList.remove('color6');
-      icon.classList.remove('color9');
-      icon.classList.add('color6');
+      icon.classList.remove("color3");
+      icon.classList.remove("color6");
+      icon.classList.remove("color9");
+      icon.classList.add("color6");
     }
-  } else if (data.palette === '3') {
-    cardName.classList.remove('color1');
-    cardName.classList.remove('color4');
-    cardName.classList.remove('color7');
-    cardName.classList.add('color7');
+  } else if (data.palette === "3") {
+    cardName.classList.remove("color1");
+    cardName.classList.remove("color4");
+    cardName.classList.remove("color7");
+    cardName.classList.add("color7");
     //cardRectangle es el palito
-    cardRectangle.classList.remove('color2');
-    cardRectangle.classList.remove('color5');
-    cardRectangle.classList.remove('color8');
-    cardRectangle.classList.add('color8');
+    cardRectangle.classList.remove("color2");
+    cardRectangle.classList.remove("color5");
+    cardRectangle.classList.remove("color8");
+    cardRectangle.classList.add("color8");
     //icons son los iconos
     for (const icon of cardIcons) {
-      console.log('entra al for');
+      console.log("entra al for");
       //color del icono
-      icon.classList.remove('color1');
-      icon.classList.remove('color4');
-      icon.classList.remove('color7');
-      icon.classList.add('color7');
+      icon.classList.remove("color1");
+      icon.classList.remove("color4");
+      icon.classList.remove("color7");
+      icon.classList.add("color7");
       //color del borde del icono
-      icon.classList.remove('color3');
-      icon.classList.remove('color6');
-      icon.classList.remove('color9');
-      icon.classList.add('color9');
+      icon.classList.remove("color3");
+      icon.classList.remove("color6");
+      icon.classList.remove("color9");
+      icon.classList.add("color9");
     }
   }
 }
 
 // applyColor(data);
 
-form.addEventListener('change', handleFillData);
+form.addEventListener("change", handleFillData);
