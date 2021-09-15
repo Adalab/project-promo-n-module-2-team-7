2a215b1e96fd7f3822b9a5ef447e6f4741fa43e8
patricuismart <patricuismart@gmail.com>
2021-09-15 18:21:47 +0200

    Clean code

--- src/js/00_collapsable.js
+++ src/js/00_collapsable.js
@@ -4,19 +4,15 @@
 const fieldset = document.querySelectorAll('.js_fieldset'); //clase en design.html
 const collapsable = document.querySelectorAll('.js_collapsable'); //clase en header-collapsable.html
 console.log('Hola Mundo');
-//handler
 
 function handleClick(ev) {
-
   let clickedHeader = ev.currentTarget;
-  // console.log(clickedHeader);
   const clicked = clickedHeader.parentNode;
   for (let index = 0; index < collapsable.length; index++) {
-    if(clickedHeader === collapsable[index]){
+    if (clickedHeader === collapsable[index]) {
       fieldset[index].classList.toggle('hidden');
       collapsable[index].classList.toggle('icon-end--rotate');
-    }
-    else {
+    } else {
       fieldset[index].classList.add('hidden');
     }
   }
